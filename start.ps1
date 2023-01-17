e4f442849349ded6356445d1ce37204f4a20be37
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2023-01-17 16:32:50 +0100

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,28 +1,37 @@
 # Prepare
     # Nuget
-        $packageProviders = Get-PackageProvider | Select-Object name
-        if(!($packageProviders.name -contains "nuget")){Install-PackageProvider -Name NuGet -RequiredVersion 2.8.5.208 -Force -Scope CurrentUser}
-        if($packageProviders -contains "nuget"){Import-PackageProvider -Name NuGet -RequiredVersion 2.8.5.208 -Force -Scope CurrentUser}
+    $packageProviders = Get-PackageProvider | Select-Object name
+    if(!($packageProviders.name -contains "nuget")){Install-PackageProvider -Name NuGet -RequiredVersion 2.8.5.208 -Force -Scope CurrentUser}
+    if($packageProviders -contains "nuget"){Import-PackageProvider -Name NuGet -RequiredVersion 2.8.5.208 -Force -Scope CurrentUser}
 
-    # TLS upgrade
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-        
-    # Disable Explorer first run
-        If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main")) {
-        New-Item -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Force | Out-Null}
-        Set-ItemProperty -Path  "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize"  -Value 1
+# TLS upgrade
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    
+# Disable Explorer first run
+    If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main")) {
+    New-Item -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Force | Out-Null}
+    Set-ItemProperty -Path  "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize"  -Value 1
 
-    $dst = $env:PSmodulepath.split(";")[1]
+# install modules
+    $modulepath = $env:PSmodulepath.split(";")[1].Replace("\","/")
 
-$modules = @(
+    $modules = @(
     "https://raw.githubusercontent.com/Andreas6920/Other/main/modules/upload.psm1";
 
-
-	)
-foreach ($module in $modules) {
-    Write-host "Downloading module:"(split-path $module -Leaf)
-    $dst = join-path $dst -ChildPath  (split-path $module -Leaf)
-    (New-Object net.webclient).Downloadfile("$module", "$dst")
-    Install-Module $dst -Force
+                )   
+    foreach ($module in $modules) {
+    $name = (split-path $module -Leaf).Replace(".psm1","").Replace(".ps1","").Replace(".psd","")
+    $dir = $modulepath+"/"+$name+"/"
+    New-Item -ItemType Directory -Path $dir -Force | Out-Null
+    $module
+    $dir
+    (New-Object net.webclient).Downloadfile("$module", "$dir")
+    
 
     }
+
+       
+       (New-Object net.webclient).Downloadfile("https://raw.githubusercontent.com/Andreas6920/Other/main/modules/upload.psm1", "C:/temp")
+        
+
+        
