9a5ff36d0802b0cf74e50a166df5717d9d58e1bf
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2023-01-17 15:10:14 +0100

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,8 +1,21 @@
-# Install modules
-$dst = $env:PSmodulepath.split(";")[1]
+# Prepare
+
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main")) {
+    New-Item -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Force | Out-Null}
+    Set-ItemProperty -Path  "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize"  -Value 1
+
+    $dst = $env:PSmodulepath.split(";")[1]
+
 $modules = @(
     "https://raw.githubusercontent.com/Andreas6920/Other/main/modules/upload.psm1";
-    
+
 
 	)
-foreach ($module in $modules) {(New-Object net.webclient).Downloadfile("$_", "$dst")}
+foreach ($module in $modules) {
+    Write-host "Downloading module:"(split-path $module -Leaf)
+    $dst = join-path $dst -ChildPath  (split-path $module -Leaf)
+    (New-Object net.webclient).Downloadfile("$module", "$dst")
+    Install-Module $dst -Force
+
+    }
