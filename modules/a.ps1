a6e4349abe8178c6fe9d5c76dc5da380e76a9f9f
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2023-01-19 00:58:11 +0100

    dsfdsfds

--- modules/a.ps1
+++ modules/a.ps1
@@ -1,39 +1,53 @@
-﻿
+﻿#Install apps
+function Install-Application {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Name,
+        [Parameter(Mandatory=$true)]
+        [string]$App,
+        [Parameter(Mandatory=$false)]
+        [switch]$Ignorehash)
+
+        # Check for Chocolatey
+        $chocodir = [Environment]::GetFolderPath("CommonApplicationData")
+        $chocodir = Join-Path $chocodir -ChildPath "Chocolatey"
+        If (!(Test-Path $chocodir)) {
+        Clear-Host; Set-ExecutionPolicy Bypass -Scope Process -Force;
+        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'));
+        Clear-Host; Write-host "`tInstalling apps:" -f green;}
+        
+        # Windows notification
+        Add-Type -AssemblyName System.Windows.Forms
+        $global:balmsg = New-Object System.Windows.Forms.NotifyIcon
+        $path = (Get-Process -id $pid).Path
+        $balmsg.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
+        $balmsg.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Info
+        $balmsg.BalloonTipText = 'Installing ' + $Name 
+        $balmsg.BalloonTipTitle = "Winoptimizer"
+        $balmsg.Visible = $true
+        $balmsg.ShowBalloonTip(20000)
+
+        # Install
+        if ($Ignorehash){$app = $app + "--ignore checksum"}
+        Write-host "`t    - Installing: $Name" -f Yellow
+        choco install $App -y | Out-Null}
+
+    Write-host "`tInstalling apps:" -f green
+    Install-Application -Name "7-Zip" -App "7zip"
+    Install-Application -Name "VLC" -App "vlc"
+    Install-Application -Name "ShareX" -App "sharex"
+        Stop-Process -Name sharex
+        iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Andreas6920/Other/main/modules/sha.ps1'))
+    Install-Application -Name "Libre Wolf" -App "librewolf"
+        iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Andreas6920/Other/main/modules/lib.ps1'))
+    Install-Application -Name "Brave" -App "brave"
+    Install-Application -Name "Google Chrome" -App "googlechrome" -ignorehash
+    Install-Application -Name "Notepad++" -App "notepadplusplus"
+    Install-Application -Name "PuTTY" -App "putty"
+    Install-Application -Name "Visual Studio Code" -App "vscode"
+    Install-Application -Name "Gimp" -App "gimp"
+    Install-Application -Name "Spotify" -App "spotify"
+    Install-Application -Name "Github Desktop" -App "github-desktop"
 
 
-
-# Install Librewolf settings
-    $link = "https://drive.google.com/uc?export=download&confirm=uc-download-link&id=1m8TZD7d9nc-Vkmv1PgyyXwUMuNgFrI3s"
-    $src = "$env:TMP\librewolfprofile.zip"
-    write-host "downloading to $src..." -f green
-    (New-Object net.webclient).Downloadfile($link, $src); Start-Sleep -s 2
-
-    write-host "Starting Libre Wolf..." -f green
-    Start-Process "C:\Program Files\LibreWolf\librewolf.exe"
-    Start-Sleep -s 5
-    Stop-Process -name "librewolf"
-
-    $dst = [Environment]::GetFolderPath("ApplicationData")
-    $dst = Join-path $dst -ChildPath "librewolf\Profiles"
-    $dst = (gci $dst | ? name -match "-default?$").FullName
-
-    Start-Sleep -s 10
-    write-host "Extracting from $src..." -f green
-    write-host "Extracting to $dst..." -f green
-
-    Expand-Archive -Path $src -DestinationPath $dst -Force
-
-# Install ShareX settings
-    $link = "https://drive.google.com/uc?export=download&confirm=uc-download-link&id=13xOvgOXgOkdLuGG-JqQiVQla6Gz6Sria"
-    $src = "$env:TMP\sharexprofile.zip"
-    write-host "downloading to $src..." -f green
-    (New-Object net.webclient).Downloadfile($link, $src);
-
-    $dst = [Environment]::GetFolderPath("MyDocuments")
-    $dst = Join-path $dst -ChildPath "Sharex"
-    $dst = (gci $dst | ? name -match "-default?$").FullName
-
-    write-host "Extracting from $src..." -f green
-    write-host "Extracting to $dst..." -f green
-
-    Expand-Archive -Path $src -DestinationPath $dst -Force
