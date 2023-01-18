fd4aae4246ed9e55069106074f9a6169907397aa
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2023-01-19 00:49:04 +0100

    asd

--- modules/sha.ps1
+++ modules/sha.ps1
@@ -1,14 +1,10 @@
 ﻿# Install ShareX settings
 $link = "https://drive.google.com/uc?export=download&confirm=uc-download-link&id=13xOvgOXgOkdLuGG-JqQiVQla6Gz6Sria"
 $src = "$env:TMP\sharexprofile.zip"
-write-host "downloading to $src..." -f green
+write-host "`t`t`t- Downloading to settings" -f Yellow
 (New-Object net.webclient).Downloadfile($link, $src);
 
+write-host "`t`t`t- Importing settings" -f Yellow
 $dst = [Environment]::GetFolderPath("MyDocuments")
 $dst = Join-path $dst -ChildPath "Sharex"
-$dst = (gci $dst | ? name -match "-default?$").FullName
-
-write-host "Extracting from $src..." -f green
-write-host "Extracting to $dst..." -f green
-
 Expand-Archive -Path $src -DestinationPath $dst -Force
