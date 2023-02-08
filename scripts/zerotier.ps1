3041fcc88075d317842d335120c066492470852a
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2023-02-08 21:16:37 +0100

    Update zerotier.ps1

--- scripts/zerotier.ps1
+++ scripts/zerotier.ps1
@@ -1,7 +1,7 @@
 ﻿# Set-ExecutionPolicy    
     Set-ExecutionPolicy Bypass -Scope Process -Force;
     $sys = [Environment]::GetFolderPath("ProgramFilesX86"), [Environment]::GetFolderPath("ProgramFiles")
-    $check = Get-ChildItem $sys -Directory -Depth 0 | Where {$_.name -eq "ZeroTier"}
+    $check = Get-ChildItem $sys -Directory -Depth 0 | Where-Object {$_.name -eq "ZeroTier"}
 
 # if not installed, install ZeroTier
     if(!($check)){
@@ -19,7 +19,7 @@
     Start-Process -WindowStyle Hidden -FilePath $client -ArgumentList "join $networkid && exit" -Wait
 
 # Copy to startup
-    $shortcut = (Get-ChildItem $sys -Depth 2 | Where {$_.name -like "zerotier_desktop_ui.exe"}).FullName
+    $shortcut = (Get-ChildItem $sys -Depth 2 | Where-Object {$_.name -like "zerotier_desktop_ui.exe"}).FullName
     $startup = [Environment]::GetFolderPath("Startup")
     $startup = Join-Path -Path $startup -ChildPath (Split-Path $shortcut -Leaf)
     
