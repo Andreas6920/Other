8e7ff37f023b71d72a10fef671eb52f999f5f796
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2023-02-20 22:17:55 +0100

    Update scripts.ps1

--- scripts/scripts.ps1
+++ scripts/scripts.ps1
@@ -1,6 +1,7 @@
 ﻿do {
     Write-Host "`tScripts:" -f Yellow;"";
     Write-Host "`t[1] - Zerotier"
+    Write-Host "`t[2] - IP"
     "";
     Write-Host "`t[0] - Exit"
     Write-Host ""; Write-Host "";
@@ -9,9 +10,9 @@
     Switch ($option) { 
         0 {exit}
         1 {Clear-Host; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Andreas6920/Other/main/scripts/zerotier.ps1'))}
-        
+        2 {Clear-Host; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Andreas6920/Other/main/scripts/ip.ps1'))}
         Default {} 
     }
         
 }
-while ($option -ne 1 )
+while ($option -ne 2 )
