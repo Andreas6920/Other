6eeadb43fd9f6ae6b6cc24a4ad918a35b0f74ea7
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2023-11-21 12:31:56 +0100

    Update action.ps1

--- scripts/action.ps1
+++ scripts/action.ps1
@@ -1,4 +1,8 @@
-﻿$link = "https://app.eu.action1.com/agent/51fced32-7e39-11ee-b2da-3151362a23c3/Windows/agent(My_Organization).msi"
+﻿If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main")) {
+    New-Item -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Force | Out-Null}
+    Set-ItemProperty -Path  "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize"  -Value 1
+
+$link = "https://app.eu.action1.com/agent/51fced32-7e39-11ee-b2da-3151362a23c3/Windows/agent(My_Organization).msi"
 $path = join-path -Path $env:TMP -ChildPath (split-path $link -Leaf)
-(New-Object net.webclient).Downloadfile("$link", "$path")
+(New-Object net.webclient).Downloadfile("$link", "$path") | Out-Null
 msiexec /i $path /quiet
