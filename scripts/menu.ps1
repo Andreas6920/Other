8dfb9dbf74856bc53f11f0830b0f99f2f9560a54
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2023-12-07 13:39:29 +0100

    Update menu.ps1

--- scripts/menu.ps1
+++ scripts/menu.ps1
@@ -7,32 +7,7 @@
 
 # Setup Menu Requirements
 
-    # Create Folder
-        Write-host "Installing menu.."
-        $rootpath = [Environment]::GetFolderPath("CommonApplicationData")
-        $applicationpath = Join-path -Path $rootpath -Childpath "WinOptimizer"
-
-        if(!(Test-Path $applicationpath)){Write-host "`t- Creating folder.."; New-Item -ItemType Directory -Path $applicationpath -Force | Out-Null}
-
-    # Download Scripts
-        $scripts = @(
-        "https://raw.githubusercontent.com/Andreas6920/Other/main/scripts/win_antibloat.ps1"
-        "https://raw.githubusercontent.com/Andreas6920/Other/main/scripts/win_security.ps1"
-        "https://raw.githubusercontent.com/Andreas6920/Other/main/scripts/win_settings.ps1")
-        Write-host "`t- Loading system.."
-        Foreach ($script in $scripts) {
-
-        # Download Scripts
-            $filename = split-path $script -Leaf
-            $filedestination = join-path $applicationpath -Childpath $filename
-            (New-Object net.webclient).Downloadfile("$script", "$filedestination")
-            
-        #Creating Missing Regpath
-            $reg_install = "HKLM:\Software\WinOptimizer"
-            If(!(Test-Path $reg_install)) {New-Item -Path $reg_install -Force | Out-Null;}
-
-        #Creating Missing Regkeys
-            if (!((Get-Item -Path $reg_install).Property -match $filename)){Set-ItemProperty -Path $reg_install -Name $filename -Type String -Value 0}}
+    
 
 # Start Menu
 
