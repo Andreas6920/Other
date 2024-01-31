fd77eb843f59079af95a139b55a84431584b831d
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2024-02-01 00:25:03 +0100

    Update download-office.ps1

--- scripts/download-office.ps1
+++ scripts/download-office.ps1
@@ -4,6 +4,6 @@ Write-Host "Microsoft Office 2016 Professional" -f Yellow
 Write-Host "Danish or English? (y/n)" -nonewline;
 $Readhost = Read-Host " " 
     Switch ($ReadHost) { 
-       danish {Start "https://officecdn.microsoft.com/db/492350F6-3A01-4F97-B9C0-C7C6DDF67D60/media/da-DK/ProfessionalRetail.img";} 
-       english {Start "https://officecdn.microsoft.com/db/492350F6-3A01-4F97-B9C0-C7C6DDF67D60/media/en-US/ProfessionalRetail.img";} 
+       danish {Start "https://officecdn.microsoft.com/db/492350F6-3A01-4F97-B9C0-C7C6DDF67D60/media/da-DK/HomeBusinessRetail.img";} 
+       english {Start "https://officecdn.microsoft.com/db/492350F6-3A01-4F97-B9C0-C7C6DDF67D60/media/en-US/HomeBusinessRetail.img";} 
      } } While($Readhost -notin "danish", "english")
