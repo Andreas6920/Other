c802865e8cde9b6b47e2f026bfdc34d9a13a729e
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2024-01-31 23:59:29 +0100

    Create download-office.ps1

---
+++ scripts/download-office.ps1
@@ -0,0 +1,9 @@
+﻿Do {
+CLS
+Write-Host "Microsoft Office 2016 Professional" -f Yellow
+Write-Host "Danish or English? (y/n)" -nonewline;
+$Readhost = Read-Host " " 
+    Switch ($ReadHost) { 
+       danish {Start "https://officecdn.microsoft.com/db/492350F6-3A01-4F97-B9C0-C7C6DDF67D60/media/da-DK/ProfessionalRetail.img";} 
+       english {Start "https://officecdn.microsoft.com/db/492350F6-3A01-4F97-B9C0-C7C6DDF67D60/media/en-US/ProfessionalRetail.img";} 
+     } } While($Readhost -notin "danish", "english")
