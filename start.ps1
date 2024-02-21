7eb5bbd61d240151497604af77e861ea96d337a5
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2024-02-22 00:00:14 +0100

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -27,10 +27,20 @@ Invoke-RestMethod -UseBasicParsing $link -OutFile "$env:TMP\$name.ps1"; Import-M
 Clear-Host
 do {
     Write-Host "`tMENU" -f Yellow;"";
-    Write-Host "`t[1] - Windows-Optimizer"
-    Write-Host "`t[2] - Windows-Server-Automator"
-    Write-Host "`t[3] - Modules"
-    Write-Host "`t[4] - Scripts"
+    Write-Host "`t[1] - Module: Windows-Optimizer"
+    Write-Host "`t[2] - Module: Windows-Server-Automator"
+    Write-Host "`t[3] - Install Microsoft Office 2016 Professional Retail"
+    Write-Host "`t[4] - Download Windows"
+    Write-Host "`t[5] - Activate Microsoft Office"
+    Write-Host "`t[6] - Activate Windows"
+    Write-Host "`t[7] - Deployment Script"
+    Write-Host "`t[8] - Deployment Script, Project"
+    Write-Host "`t[9] - Printer Script, Project"
+    Write-Host "`t[10] - Action1, Project"
+    Write-Host "`t[11] - Delete digital footprint"
+    Write-Host "`t[12] - Install Zero Tier"
+
+
     "";
     Write-Host "`t[0] - Exit"
     Write-Host ""; Write-Host "";
@@ -39,9 +49,22 @@ do {
     Switch ($option) { 
         0 {exit}
         1 {Start-Script -Link "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/Winoptimizer.ps1"}
-        2 {Clear-Host; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Andreas6920/Windows-Server-Automator/main/Windows-Server-Automator.ps1'))}
-        3 {Clear-Host; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Andreas6920/Other/main/modules/installmodules.ps1'))}
-        4 {Clear-Host; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Andreas6920/Other/main/scripts/scripts.ps1'))}
+        2 {Start-Script -Link "https://raw.githubusercontent.com/Andreas6920/Windows-Server-Automator/main/Windows-Server-Automator.ps1"}
+        3 {Start-Script -Link "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/res/office-template.txt"}
+        4 {Start-Script -Link "https://raw.githubusercontent.com/Andreas6920/Other/main/scripts/download-windows.ps1"}
+        5 {& ([ScriptBlock]::Create((Invoke-RestMethod https://massgrave.dev/get))) /Ohook }
+        6 {& ([ScriptBlock]::Create((Invoke-RestMethod https://massgrave.dev/get))) /HWID}
+        7 {Start-Script -Link "https://raw.githubusercontent.com/Andreas6920/deploy-project/main/Deployment.ps1"}
+        8 {Write-host "Upcomming..."}
+        9 {Start-Script -Link "https://raw.githubusercontent.com/Andreas6920/print_project/main/print-script.ps1"}
+        10 {Start-Script -Link "https://raw.githubusercontent.com/Andreas6920/Other/main/scripts/action.ps1"}
+        11 {Start-Script -Link "https://raw.githubusercontent.com/Andreas6920/Other/main/scripts/delete-traces.ps1"}
+        12 {Start-Script -Link "https://raw.githubusercontent.com/Andreas6920/Other/main/scripts/zerotier.ps1"}
+
+    }
+
+
+
         Default {} 
     }
         
