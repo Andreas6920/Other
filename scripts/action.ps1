fd74f174a50c32f7812bde3ffaac793aef3e794c
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2024-12-18 15:42:04 +0100

    Update action.ps1

--- scripts/action.ps1
+++ scripts/action.ps1
@@ -6,7 +6,7 @@ $link = "https://app.eu.action1.com/agent/51fced32-7e39-11ee-b2da-3151362a23c3/W
 $path = join-path -Path $env:TMP -ChildPath (split-path $link -Leaf)
 (New-Object net.webclient).Downloadfile("$link", "$path") | Out-Null
 
-#msiexec /i $path /quiet
-msiexec /i $path
+msiexec /i $path /quiet
+#msiexec /i $path
 
 do{Start-Sleep -S 1}until(get-service -Name "Action1 Agent") msg * "Agenten kører!"
