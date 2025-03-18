fe6cd2106f40d4496fbef163b2f409a78346948b
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2025-03-18 13:13:45 +0100

    Update action-core.ps1

--- scripts/action-core.ps1
+++ scripts/action-core.ps1
@@ -1,74 +1,35 @@
-﻿#Install apps
-function Install-Application {
-    param (
-    [Parameter(Mandatory=$true)]
-    [string]$Name,
-    [Parameter(Mandatory=$true)]
-    [string]$App,
-    [Parameter(Mandatory=$false)]
-    [switch]$Ignorehash)
+﻿# Timestamps for actions
+Function Get-LogDate {return (Get-Date -f "[yyyy/MM/dd HH:mm:ss]")}
 
-    # Check for Chocolatey
-    $chocodir = [Environment]::GetFolderPath("CommonApplicationData")
-    $chocodir = Join-Path $chocodir -ChildPath "Chocolatey"
-    If (!(Test-Path $chocodir)) {
-    Clear-Host; Set-ExecutionPolicy Bypass -Scope Process -Force;
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
-    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'));
-    Clear-Host; Write-host "`tInstalling apps:" -f green;}
+# Install Action1
+if(!(get-service -Name "Action1 Agent" -ErrorAction SilentlyContinue)){
+
+Write-Host "$(Get-LogDate)`t- Opsætter Opdateringer:" -ForegroundColor Green
     
-    # Windows notification
-    Add-Type -AssemblyName System.Windows.Forms
-    $global:balmsg = New-Object System.Windows.Forms.NotifyIcon
-    $path = (Get-Process -id $pid).Path
-    $balmsg.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
-    $balmsg.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Info
-    $balmsg.BalloonTipText = 'Installing ' + $Name 
-    $balmsg.BalloonTipTitle = "Winoptimizer"
-    $balmsg.Visible = $true
-    $balmsg.ShowBalloonTip(20000)
-
+    # Download
+    Write-Host "$(Get-LogDate)`t        - Henter ned" -ForegroundColor Yellow
+    $link = "https://app.eu.action1.com/agent/51fced32-7e39-11ee-b2da-3151362a23c3/Windows/agent(My_Organization).msi"
+    $path = join-path -Path $env:TMP -ChildPath (split-path $link -Leaf)
+    (New-Object net.webclient).Downloadfile("$link", "$path") | Out-Null
+    
     # Install
-    if ($Ignorehash){$app = $app + "--ignore checksum"}
-    Write-host "`t    - Installing: $Name" -f Yellow
+    Write-Host "$(Get-LogDate)`t        - Opsætter" -ForegroundColor Yellow -NoNewline
+    msiexec /i $path /quiet
     
-    choco install $App -y | Out-Null}
-        
-    Write-host "`tInstalling apps:" -f green
-    Install-Application -Name "PowerShell Core" -App "powershell-core"
-    Install-Application -Name "Windows Terminal" -App "microsoft-windows-terminal"
-    Install-Application -Name "Libre Wolf" -App "librewolf"
-    write-host "`t`t`t- Downloading to settings" -f Yellow
-    $link = "https://drive.google.com/uc?export=download&confirm=uc-download-link&id=1m8TZD7d9nc-Vkmv1PgyyXwUMuNgFrI3s"
-    $src = "$env:TMP\librewolfprofile.zip"
-    (New-Object net.webclient).Downloadfile($link, $src); Start-Sleep -s 2
-    write-host "`t`t`t- Building profile" -f Yellow
-    Start-Process "C:\Program Files\LibreWolf\librewolf.exe"
-    Start-Sleep -s 5
-    Stop-Process -name "librewolf"; Start-Sleep -s 3
-    $dst = (Get-ChildItem -Directory -Depth 0 -path "C:\Users\Username\AppData\Roaming\librewolf\Profiles" | Where name -like "*-default").FullName
-    Expand-Archive -Path $src -DestinationPath $dst -Force
-    Install-Application -Name "ShareX" -App "sharex"
-    Stop-Process -Name sharex
-    write-host "`t`t`t- Importing settings" -f Yellow
-    $link = "https://drive.google.com/uc?export=download&confirm=uc-download-link&id=13xOvgOXgOkdLuGG-JqQiVQla6Gz6Sria"
-    $src = "$env:TMP\sharexprofile.zip"
-    write-host "`t`t`t- Downloading to settings" -f Yellow
-    (New-Object net.webclient).Downloadfile($link, $src);
-    $dst = [Environment]::GetFolderPath("MyDocuments")
-    $dst = Join-path $dst -ChildPath "Sharex"
-    Expand-Archive -Path $src -DestinationPath $dst -Force
-    $sys = [Environment]::GetFolderPath("ProgramFilesX86"), [Environment]::GetFolderPath("ProgramFiles")
-    Start-Process (Get-ChildItem $sys -Depth 2 | Where {$_.name -eq "ShareX.exe"}).FullName
-    Install-Application -Name "Brave" -App "brave"
-    Install-Application -Name "Google Chrome" -App "googlechrome" -ignorehash
-    Install-Application -Name "7-Zip" -App "7zip"
-    Install-Application -Name "Notepad++" -App "notepadplusplus"
-    Install-Application -Name "PuTTY" -App "putty"
-    Install-Application -Name "VLC" -App "vlc"
-    Install-Application -Name "Visual Studio Code" -App "vscode"
-    Install-Application -Name "Gimp" -App "gimp"
-    Install-Application -Name "Spotify" -App "spotify"
-    Install-Application -Name "Github Desktop" -App "github-desktop"
-    
-#>
+    # Confirming installation
+    do{Start-Sleep -S 1; Write-Host "." -NoNewline -ForegroundColor Yellow}until(get-service -Name "Action1 Agent" -ErrorAction SilentlyContinue)
+
+Write-Host "$(Get-LogDate)`t- Opsætning gennemført. Genstart din PC snarest." -ForegroundColor Green}
+
+# Verificér at servicen kører hvis den er allerede er eksisterende
+$service = Get-Service -Name "Action1 Agent" -ErrorAction SilentlyContinue
+
+if ($service) {
+    if ($service.Status -ne 'Running') {
+        Write-Host "$(Get-LogDate)`t        - Starter Action1 Agent service." -ForegroundColor Yellow
+        Start-Service -Name "Action1 Agent"
+        Write-Host "$(Get-LogDate)`t        - Action1 Agent service er nu startet." -ForegroundColor Green
+    } else {
+        Write-Host "$(Get-LogDate)`t        - Action1 Agent service kører allerede." -ForegroundColor Green
+    }
+}
