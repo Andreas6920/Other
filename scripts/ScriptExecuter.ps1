1c6c08c81a5386049720b6b3037961aca2a7b910
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2025-03-19 23:00:27 +0100

    Update ScriptExecuter.ps1

--- scripts/ScriptExecuter.ps1
+++ scripts/ScriptExecuter.ps1
@@ -1,58 +1,64 @@
 # Version 1.0
 
-# Konfiguration
-    $basePath = "C:/script"
-    $executePath = "$basePath/Execute/ScriptExecuter.ps1"
-    $archivePath = "$basePath/Archive"
-    $logPath = "$basePath/Logs/ScriptLog.txt"
-    $scriptURL = "https://raw.githubusercontent.com/Andreas6920/Other/refs/heads/main/scripts/ScriptExecuter.ps1"
+# Klargør systemet
 
-# Funktion til formateret dato
-    function Get-FormattedDate {return (Get-Date).ToString("yyyy/MM/dd HH:mm:ss")}
+    # Konfiguration
+    $basepath = "C:/Script"
+    $executepath = "$basepath/Execute"
+    $executefile = "$executepath/ScriptExecuter.ps1"
+    $archivepath = "$basepath/Archive"
+    $logpath = "$basepath/Logs"
+    $logfile = "$logpath/ScriptLog.txt"
+    $scripturl = "https://raw.githubusercontent.com/Andreas6920/Other/refs/heads/main/scripts/scriptexecuter.ps1"
 
-# Tjek om nødvendige mapper findes
-    $folders = @($archivePath, "$basePath/Logs")
+    # Funktion til formateret dato
+    function get-logdate {return (Get-Date).ToString("yyyy/MM/dd HH:mm:ss")}
+
+    # Kontrollere at mapperne eksistere
+    $folders = @($archivepath, $logpath)
     foreach ($folder in $folders) {if (!(Test-Path $folder)) {New-Item -ItemType Directory -Path $folder -Force | Out-Null}}
 
-# Tjek om Executer skal opdateres
+# ScriptExecuter opdatering
 
-    # Hent online scripts første linje (tag højde for æ, ø, å)
-        try {$onlineScript = [System.Text.Encoding]::UTF8.GetString((New-Object System.Net.WebClient).DownloadData($scriptURL))
-            $onlineFirstLine = ($onlineScript -split "`r?`n")[0]} 
-        catch {"[$(Get-FormattedDate)] Fejl ved hentning af script fra $scriptURL - $_" | Add-Content -Path $logPath; exit 1}
+    # Hent online scriptets første linje (Tager højde for æ, ø, å)
+        try {   $onlinescript = [System.Text.Encoding]::UTF8.GetString((New-Object System.Net.WebClient).DownloadData($scripturl))
+                $onlinefirstline = ($onlinescript -split "`r?`n")[0]}
+        catch { "[$(get-logdate)] Fejl ved hentning af script fra $scripturl - $_" | Add-Content -Path $logfile;}
 
     # Hent lokal scriptets første linje
-        if (Test-Path $executePath) {$localFirstLine = (Get-Content $executePath -First 1 -Encoding UTF8)}
-        else {$localFirstLine = ""}
+        if (Test-Path $executefile) { $localfirstline = (Get-Content $executefile -First 1 -Encoding UTF8)}
+        else { $localfirstline = ""}
 
     # Opdater scriptet, hvis online versionen er anderledes
-    if ($onlineFirstLine -ne $localFirstLine){
-        $onlineScript | Set-Content -Path $executePath -Encoding UTF8
-        "[$(Get-FormattedDate)] Executer Opdatering fundet:" | Add-Content -Path $logPath
-        "[$(Get-FormattedDate)] Online Version: $onlinescript" | Add-Content -Path $logPath
-        "[$(Get-FormattedDate)] Lokal Version: $localFirstLine" | Add-Content -Path $logPath
-        "[$(Get-FormattedDate)] Script opdateret fra Github" | Add-Content -Path $logPath}
+        if ($onlinefirstline -ne $localfirstline) { 
+            "[$(get-logdate)] Opdatering fundet:" | Add-Content -Path $logfile
+            "[$(get-logdate)] `tOnline version: $onlinefirstline" | Add-Content -Path $logfile
+            "[$(get-logdate)] `tLokal version: $localfirstline" | Add-Content -Path $logfile
+            $onlinescript | Set-Content -Path $executefile -Encoding UTF8
+            "[$(get-logdate)] Script opdateret fra online kilde" | Add-Content -Path $logfile}
 
-# Tjek for scripts i C:/script
-    $scripts = Get-ChildItem -Path $basePath -Filter "*.ps1" -File | Where-Object { $_.FullName -ne $executePath }
+# Kør Script
 
-    # Kør script hvis der ligger nogle nye i rodmappen
+    # Tjek for nye scripts i C:/script
+    $scripts = Get-ChildItem -Path $basepath -Filter "*.ps1" -File
+
+    # Kør scriptet
     if ($scripts.Count -gt 0) {
         $timestamp = Get-Date -Format "yyyy.MM.dd-HH.mm.ss"
-        $archiveSubPath = "$archivePath/$timestamp"
-        New-Item -ItemType Directory -Path $archiveSubPath -Force | Out-Null
+        $archivesubpath = "$archivepath/$timestamp"
+        New-Item -ItemType Directory -Path $archivesubpath -Force | Out-Null
         
         foreach ($script in $scripts) {
-            try {$startTime = Get-FormattedDate
-                "[$startTime] Kører script: $($script.Name)" | Add-Content -Path $logPath
+            try {   $starttime = get-logdate
+                    "[$starttime] Kører script: $($script.Name)" | Add-Content -Path $logfile
+
+                    # Kør Script               
+                    & $script.FullName
                 
-                & $script.FullName
+                    $endtime = get-logdate
+                    "[$endtime] Færdig med at køre script: $($script.Name)" | Add-Content -Path $logfile
                 
-                $endTime = Get-FormattedDate
-                "[$endTime] Færdig med at køre script: $($script.Name)" | Add-Content -Path $logPath
-                Move-Item -Path $script.FullName -Destination "$archiveSubPath/$($script.Name)" -Force}
-            catch {"[$(Get-FormattedDate)] Fejl ved kørsel af script: $($script.Name) - $_" | Add-Content -Path $logPath}
-        }
-    } 
-    
-    else {"[$(Get-FormattedDate)] Ingen scripts fundet i $basePath" | Add-Content -Path $logPath}
+                    Move-Item -Path $script.FullName -Destination "$archivesubpath/$($script.Name)" -Force
+                    "[$(get-logdate)] Script flyttet til: $(Resolve-Path $archivesubpath)" | Add-Content -Path $logfile}
+            catch { "[$(get-logdate)] Fejl ved kørsel af script: $($script.Name) - $_" | Add-Content -Path $logfile}}} 
+    else { "[$(get-logdate)] Ingen scripts fundet i $(Resolve-Path $basepath)" | Add-Content -Path $logfile }
