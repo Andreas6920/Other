871f693b623ec7fae47552b0b939505879f52c64
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2025-04-22 12:27:19 +0200

    Update SDI-Tool.ps1

--- scripts/SDI-Tool.ps1
+++ scripts/SDI-Tool.ps1
@@ -6,6 +6,7 @@ Function Get-LogDate {return (Get-Date -f "[yyyy/MM/dd HH:mm:ss]")}
 
 $url = "https://sdi-tool.org/download/"
 $FolderPath = [System.IO.Path]::Combine([environment]::GetFolderPath("CommonApplicationData"), "SDI Tool")
+$ZipFilePath = "$FolderPath\SDI_Tool.zip"
 
 
 # Opret mappen, hvis den ikke allerede eksisterer
@@ -13,6 +14,7 @@ if (!(Test-Path -Path $FolderPath)) {
     New-Item -ItemType Directory -Path $FolderPath | Out-Null;
     Write-Host "$(Get-LogDate)`t    Opretter mappe til download på skrivebordet..." -ForegroundColor Green}
 
+# Hent programmet online
 # Hent HTML-indholdet med Invoke-RestMethod
 $response = Invoke-RestMethod -Uri $url -Method Get
 Write-Host "$(Get-LogDate)`t    Henter HTML-indhold fra: $url" -ForegroundColor Green
@@ -21,26 +23,22 @@ Write-Host "$(Get-LogDate)`t    Henter HTML-indhold fra: $url" -ForegroundColor 
 $content = $response | Out-String
 
 # Find linket til zip-filen ved hjælp af Regex
-$linkPattern = "https:\/\/sdi-tool\.org\/releases\/SDI_R\d{4}\.zip"
+$linkPattern = "https:\/\/sdi-tool\.org\/releases\/SDI_R\d{3-4}\.zip"
 $match = [regex]::Match($content, $linkPattern)
 
 if ($match.Success) {
     $zipUrl = $match.Value
-    Write-Host "$(Get-LogDate)`t    Fundet download-link: $zipUrl" -ForegroundColor Green
-
-    # Definer stien hvor ZIP-filen skal downloades
-    $zipFilePath = "$FolderPath\SDI_Tool.zip"
-
     # Download ZIP-filen
-    Invoke-WebRequest -Uri $zipUrl -OutFile $zipFilePath
-    Write-Host "$(Get-LogDate)`t    Filen blev downloadet til: $zipFilePath" -ForegroundColor Green
+    Write-Host "$(Get-LogDate)`t    Downloader: $zipUrl" -ForegroundColor Green
+    Invoke-WebRequest -Uri $zipUrl -OutFile $ZipFilePath
+    Write-Host "$(Get-LogDate)`t    Filen blev downloadet til: $ZipFilePath" -ForegroundColor Green
 
     # Ekstraher ZIP-filen til skrivebordsmappen
-    Expand-Archive -Path $zipFilePath -DestinationPath $FolderPath -Force
-    Write-Host "$(Get-LogDate)`t    Filen er blevet udpakket til: $FolderPath" -ForegroundColor Green
+    Write-Host "$(Get-LogDate)`t    Udpakker til: $FolderPath" -ForegroundColor Green
+    Expand-Archive -Path $ZipFilePath -DestinationPath $FolderPath -Force
 
     # Slet ZIP-filen efter udpakning
-    Remove-Item -Path $zipFilePath
+    Remove-Item -Path $ZipFilePath
     Write-Host "$(Get-LogDate)`t    ZIP-filen er blevet slettet efter udpakning." -ForegroundColor Green
 
     # Angiv sti til SDI_auto.bat filen
