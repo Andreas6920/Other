392d432dd22daf37241ef58cc015fb6c0ba6d642
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2025-04-22 11:58:42 +0200

    Update SDI-Tool.ps1

--- scripts/SDI-Tool.ps1
+++ scripts/SDI-Tool.ps1
@@ -4,15 +4,14 @@
 # Timestampfunktionen
 Function Get-LogDate {return (Get-Date -f "[yyyy/MM/dd HH:mm:ss]")}
 
-# URL til download-siden
 $url = "https://sdi-tool.org/download/"
+$FolderPath = [System.IO.Path]::Combine([environment]::GetFolderPath("CommonApplicationData"), "SDI Tool")
 
-# Angiv sti til skrivebordet
-$desktopPath = [System.IO.Path]::Combine([environment]::GetFolderPath("Desktop"), "SDI_Tool")
 
 # Opret mappen, hvis den ikke allerede eksisterer
-if (!(Test-Path -Path $desktopPath)) {New-Item -ItemType Directory -Path $desktopPath | Out-Null}
-Write-Host "$(Get-LogDate)`t    Opretter mappe til download på skrivebordet..." -ForegroundColor Green
+if (!(Test-Path -Path $FolderPath)) {
+    New-Item -ItemType Directory -Path $FolderPath | Out-Null;
+    Write-Host "$(Get-LogDate)`t    Opretter mappe til download på skrivebordet..." -ForegroundColor Green}
 
 # Hent HTML-indholdet med Invoke-RestMethod
 $response = Invoke-RestMethod -Uri $url -Method Get
@@ -30,22 +29,22 @@ if ($match.Success) {
     Write-Host "$(Get-LogDate)`t    Fundet download-link: $zipUrl" -ForegroundColor Green
 
     # Definer stien hvor ZIP-filen skal downloades
-    $zipFilePath = "$desktopPath\SDI_Tool.zip"
+    $zipFilePath = "$FolderPath\SDI_Tool.zip"
 
     # Download ZIP-filen
     Invoke-WebRequest -Uri $zipUrl -OutFile $zipFilePath
     Write-Host "$(Get-LogDate)`t    Filen blev downloadet til: $zipFilePath" -ForegroundColor Green
 
     # Ekstraher ZIP-filen til skrivebordsmappen
-    Expand-Archive -Path $zipFilePath -DestinationPath $desktopPath -Force
-    Write-Host "$(Get-LogDate)`t    Filen er blevet udpakket til: $desktopPath" -ForegroundColor Green
+    Expand-Archive -Path $zipFilePath -DestinationPath $FolderPath -Force
+    Write-Host "$(Get-LogDate)`t    Filen er blevet udpakket til: $FolderPath" -ForegroundColor Green
 
     # Slet ZIP-filen efter udpakning
     Remove-Item -Path $zipFilePath
     Write-Host "$(Get-LogDate)`t    ZIP-filen er blevet slettet efter udpakning." -ForegroundColor Green
 
     # Angiv sti til SDI_auto.bat filen
-    $batFilePath = [System.IO.Path]::Combine($desktopPath, "SDI_auto.bat")
+    $batFilePath = [System.IO.Path]::Combine($FolderPath, "SDI_auto.bat")
 
     # Tjek om SDI_auto.bat filen eksisterer
     if (Test-Path -Path $batFilePath) {
@@ -55,6 +54,6 @@ if ($match.Success) {
         Start-Process -FilePath $batFilePath -NoNewWindow -Wait
         Write-Host "$(Get-LogDate)`t    SDI_auto.bat filen er blevet kørt." -ForegroundColor Green}
     else {
-        Write-Host "$(Get-LogDate)`t    Filen SDI_auto.bat blev ikke fundet i mappen: $desktopPath" -ForegroundColor Green}} 
+        Write-Host "$(Get-LogDate)`t    Filen SDI_auto.bat blev ikke fundet i mappen: $FolderPath" -ForegroundColor Green}} 
 
 else { Write-Host "$(Get-LogDate)`t    Ingen gyldig link blev fundet på siden." -ForegroundColor Green }
