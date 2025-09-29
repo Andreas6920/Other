e81d1a217fc41c025b31b7b028d8c9ad4243cdc3
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2025-09-29 10:23:20 +0200

    Update zerotier.ps1

--- scripts/zerotier.ps1
+++ scripts/zerotier.ps1
@@ -1,27 +1,29 @@
 ﻿# Set-ExecutionPolicy    
-    Set-ExecutionPolicy Bypass -Scope Process -Force;
-    $sys = [Environment]::GetFolderPath("ProgramFilesX86"), [Environment]::GetFolderPath("ProgramFiles")
-    $check = Get-ChildItem $sys -Directory -Depth 0 | Where-Object {$_.name -eq "ZeroTier"}
+Set-ExecutionPolicy Bypass -Scope Process -Force;
+$sys = [Environment]::GetFolderPath("ProgramFilesX86"), [Environment]::GetFolderPath("ProgramFiles")
+$check = Get-ChildItem $sys -Directory -Depth 0 | Where-Object { $_.name -eq "ZeroTier" }
 
 # if not installed, Start Install
     
-    # Install Chocolatey
-        if(!($check)){
-            if (!(Test-Path "$env:ProgramData\Chocolatey")) {
-                Set-ExecutionPolicy Bypass -Scope Process -Force
-                [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-                Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))}
+# Install Chocolatey
+if (!($check)) {
+    if (!(Test-Path "$env:ProgramData\Chocolatey")) {
+        Set-ExecutionPolicy Bypass -Scope Process -Force
+        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    }
     # Install ZeroTier
-        choco install zerotier-one -y}
+    choco install zerotier-one -y
+}
 
 # Join ZeroTier network 
-    $networkid = Read-Host -Prompt "    Enter Zerotier Network id"
-    $client = "C:\Program Files (x86)\ZeroTier\One\zerotier-cli.bat";
-    Start-Process -WindowStyle Hidden -FilePath $client -ArgumentList "join $networkid && exit" -Wait
+$networkid = Read-Host -Prompt "    Enter Zerotier Network id"
+$client = "C:\Program Files (x86)\ZeroTier\One\zerotier-cli.bat";
+Start-Process -WindowStyle Hidden -FilePath $client -ArgumentList "join $networkid && exit" -Wait
 
 # Copy to startup
-    $shortcut = (Get-ChildItem $sys -Depth 2 | Where-Object {$_.name -like "zerotier_desktop_ui.exe"}).FullName
-    $startup = [Environment]::GetFolderPath("Startup")
-    $startup = Join-Path -Path $startup -ChildPath (Split-Path $shortcut -Leaf)
+$shortcut = (Get-ChildItem $sys -Depth 2 | Where-Object { $_.name -like "zerotier_desktop_ui.exe" }).FullName
+$startup = [Environment]::GetFolderPath("Startup")
+$startup = Join-Path -Path $startup -ChildPath (Split-Path $shortcut -Leaf)
     
-    Copy-Item $shortcut $startup
+Copy-Item $shortcut $startup
