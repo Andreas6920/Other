59e2327e2bab98c6a5f9f589150966c8de9ee7dd
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2026-01-25 23:55:55 +0100

    Update BDInstall.ps1

--- scripts/BDInstall.ps1
+++ scripts/BDInstall.ps1
@@ -1,35 +1,37 @@
 
-# Bitdefender Install
-    $link = https://flow.bitdefender.net/connect/2020/en_us/bitdefender_windows_6cc70ac0-d200-4ee4-a3c7-345f029c4d9d.exe
-    $File = Join-path -Path ([Environment]::GetFolderPath("Desktop")) -Childpath "Bitdefender Security.exe"
-    (New-Object net.webclient).Downloadfile($link, $File)
-    Start-Process -FilePath $File
-
 # Bitdefender Uninstall
-
-
-    
-Write-Host "`t[1]`tCreate Folder"
-Write-Host "`t[2]`tInstall Script"
-Write-Host "`t[3]`tInstall Bitdefender"
-Write-Host "`t[4]`tUinstall Bitdefender"
+Write-Host "`tENDPOINT INSTALLER:" -ForegroundColor Yellow
+Write-Host "`n`n"
+Write-Host "`t[1]`tInstall Script"
+Write-Host "`t[2]`tInstall Bitdefender"
+Write-Host "`t[3]`tUinstall Bitdefender"
+Write-Host "`n`n"
 
 While($true) {
-Write-Host "Menu:" -nonewline;
+Write-Host "`tSelect your number" -nonewline;
 $Readhost = Read-Host " "
 Switch ($ReadHost) {
-    1 {mkdir "C:\ProgramData\am" -force}
-    2 {Invoke-RestMethod "https://raw.githubusercontent.com/Andreas6920/Other/refs/heads/main/scripts/ScriptExecuterInstall.ps1" | Invoke-Expression}
-    3 { $link = https://flow.bitdefender.net/connect/2020/en_us/bitdefender_windows_6cc70ac0-d200-4ee4-a3c7-345f029c4d9d.exe
+
+    1 { 
+        $File = Join-path -Path $Env:TMP -Childpath "Install-ScriptExecuter.ps1"
+        (New-Object net.webclient).Downloadfile($link, $File)
+        . $File; Install-ScriptExecuter
+    }
+
+    2 { $link = "https://flow.bitdefender.net/connect/2020/en_us/bitdefender_windows_6cc70ac0-d200-4ee4-a3c7-345f029c4d9d.exe"
         $File = Join-path -Path ([Environment]::GetFolderPath("Desktop")) -Childpath "Bitdefender Security.exe"
+        Write-Host "`t`t`t - Downloading $File"
         (New-Object net.webclient).Downloadfile($link, $File)
-        Start-Process -FilePath $File}
-    4{  $link = "https://www.bitdefender.com/files/KnowledgeBase/file/Bitdefender_2023_Uninstall_Tool.exe"
-        $File = Join-path -Path ([Environment]::GetFolderPath("Desktop")) -Childpath "Bitdefender Uninstaller.exe"
-        (New-Object net.webclient).Downloadfile($link, $File)
-        Start-Process -FilePath $File}
+        Write-Host "`t`t`t - Starting $File"
+        Start-Process -FilePath $File
+    }
+
+    3{ $link = "https://www.bitdefender.com/files/KnowledgeBase/file/Bitdefender_2023_Uninstall_Tool.exe"
+       $File = Join-path -Path ([Environment]::GetFolderPath("Desktop")) -Childpath "Bitdefender Uninstaller.exe"
+       Write-Host "`t`t`t - Downloading $File"
+       (New-Object net.webclient).Downloadfile($link, $File)
+       Write-Host "`t`t`t - Starting $File"
+       Start-Process -FilePath $File}
  } }
 
 
-
- 
