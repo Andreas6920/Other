37102bcfa3a8e9af075f4d55550d16c8289009bb
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2026-01-31 02:50:13 +0100

    correction

--- scripts/Install-ZeroTier.ps1
+++ scripts/Install-ZeroTier.ps1
@@ -1,34 +1,29 @@
-﻿function Install-ZeroTier {
+﻿# Join zerotier ID
+    $networkid = Read-Host -Prompt "    Enter Zerotier Network id"
 
+# Test if ZeroTier is installed on this system
+    if((!(test-path "C:\Program Files (x86)\ZeroTier")) -and (!(test-path "C:\Program Files\ZeroTier"))){ 
 
+        # If winget is installed on the system - use winget to install Zerotier
+            if (Get-Command Winget -ErrorAction SilentlyContinue) {
+                    winget install --id ZeroTier.ZeroTierOne --source winget -e --force
+                    Clear-Host}
 
-# Set-ExecutionPolicy    
-Set-ExecutionPolicy Bypass -Scope Process -Force;
-$sys = [Environment]::GetFolderPath("ProgramFilesX86"), [Environment]::GetFolderPath("ProgramFiles")
-$check = Get-ChildItem $sys -Directory -Depth 0 | Where-Object { $_.name -eq "ZeroTier" }
-
-# if not installed, Start Install
-    
-# Install Chocolatey
-if (!($check)) {
-    if (!(Test-Path "$env:ProgramData\Chocolatey")) {
-        Set-ExecutionPolicy Bypass -Scope Process -Force
-        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-    }
-    # Install ZeroTier
-    choco install zerotier-one -y
-}
+        # If winget is NOT installed on the system - use chocolatey to install Zerotier
+            else {
+                # Install Chocolatey
+                    Set-ExecutionPolicy Bypass -Scope Process -Force
+                    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+                    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+                # Install zerotier
+                    choco install zerotier-one -y}}
 
 # Join ZeroTier network 
-$networkid = Read-Host -Prompt "    Enter Zerotier Network id"
-$client = "C:\Program Files (x86)\ZeroTier\One\zerotier-cli.bat";
-Start-Process -WindowStyle Hidden -FilePath $client -ArgumentList "join $networkid && exit" -Wait
+    $client = (Get-ChildItem -Path "C:\Program Files\ZeroTier", "C:\Program Files (x86)\ZeroTier" -Recurse -Filter "zerotier-cli.bat" -ErrorAction SilentlyContinue).FullName
+    Start-Process -WindowStyle Hidden -FilePath $client -ArgumentList "join $networkid && exit" -Wait
 
 # Copy to startup
-$shortcut = (Get-ChildItem $sys -Depth 2 | Where-Object { $_.name -like "zerotier_desktop_ui.exe" }).FullName
-$startup = [Environment]::GetFolderPath("Startup")
-$startup = Join-Path -Path $startup -ChildPath (Split-Path $shortcut -Leaf)
-    
-Copy-Item $shortcut $startup
-}
+    $shortcut = (Get-ChildItem -Path "C:\Program Files\ZeroTier", "C:\Program Files (x86)\ZeroTier" -Recurse -Filter "zerotier_desktop_ui.exe" -ErrorAction SilentlyContinue).FullName
+    $startup = [Environment]::GetFolderPath("Startup")
+    $startup = Join-Path -Path $startup -ChildPath (Split-Path $shortcut -Leaf)
+    Copy-Item $shortcut $startup
